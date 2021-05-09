efbc3b4a2cab5c3c9f22c59b199f6d5ab73d474f
sheemambabu <46585696+sheemambabu@users.noreply.github.com>
2021-05-09 02:09:30 -0400

    update

--- testPython.py
+++ testPython.py
@@ -1,9 +1,9 @@
 import os
 
-print(os.getcmd())
+print(os.getcwd())
 
-f.open("Testing_1.txt","a")
+f = open("Testing_1.txt","a")
 for i in range(10):
-  f.write("New Line"+(i+1))
+  f.write("This is line %d\r\n" % (i+1))
 
 f.close()
